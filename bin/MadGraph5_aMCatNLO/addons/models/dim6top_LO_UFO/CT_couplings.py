1c8132969196d8ea74ff7fdd1ab7b736f52c2bf6
Lingqiang He <he614@purdue.edu>
2026-05-08 21:08:22 +0200

    put dim6top model there

---
+++ bin/MadGraph5_aMCatNLO/addons/models/dim6top_LO_UFO/CT_couplings.py
@@ -0,0 +1,11 @@
+# This file was automatically created by FeynRules 2.3.29
+# Mathematica version: 11.1.0 for Linux x86 (64-bit) (March 13, 2017)
+# Date: Wed 6 Jun 2018 12:55:50
+
+
+from object_library import all_couplings, Coupling
+
+from function_library import complexconjugate, re, im, csc, sec, acsc, asec, cot
+
+
+
---
+++ bin/MadGraph5_aMCatNLO/addons/models/dim6top_LO_UFO/coupling_orders.py
@@ -0,0 +1,24 @@
+# This file was automatically created by FeynRules 2.3.29
+# Mathematica version: 11.1.0 for Linux x86 (64-bit) (March 13, 2017)
+# Date: Wed 6 Jun 2018 12:55:50
+
+
+from object_library import all_orders, CouplingOrder
+
+
+QCD = CouplingOrder(name = 'QCD',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
+QED = CouplingOrder(name = 'QED',
+                    expansion_order = 99,
+                    hierarchy = 2)
+
+DIM6 = CouplingOrder(name = 'DIM6',
+                     expansion_order = 99,
+                     hierarchy = 1)
+
+FCNC = CouplingOrder(name = 'FCNC',
+                     expansion_order = 99,
+                     hierarchy = 1)
+
